23d1462730299350ef62dfcca25e893481a99676
Patrick Moore <patrickwmoore88@gmail.com>
2019-12-16 20:37:56 -0600

    delete button test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const models = require("./models");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const session = require("express-session");
 const accountRouter = require("./routes/account");
 const exploreRouter = require("./routes/explore");
@@ -10,72 +10,76 @@ const bcrypt = require("bcrypt");
 app.set("view engine", "pug");
 
 app.use(
-    session({
-        secret: "La li lu le lo",
-        resave: false,
-        saveUninitialized: true
-    })
+  session({
+    secret: "La li lu le lo",
+    resave: false,
+    saveUninitialized: true
+  })
 );
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/account", accountRouter);
 app.use("/explore", exploreRouter);
 
-
-app.get('/', function (req, res) {
-    res.render("index");
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.get('/registration', function (req, res) {
-    res.render('registration');
+app.get("/registration", function(req, res) {
+  res.render("registration");
 });
 
-app.post('/registerUser', async (req, res) =>{
-    try{
-        let registeredUser = await models.users.findOne({where: {user_name: req.body.user_name}});
-        if (registeredUser) {
-            throw new Error("That username has already been taken. Booooo!");
-        }
-        let encrypted = bcrypt.hashSync(req.body.password, 10)
-            models.users.create({
-                user_name: req.body.user_name,
-                password: encrypted})     
-    }catch(e){
-        res.send(e);
+app.post("/registerUser", async (req, res) => {
+  try {
+    let registeredUser = await models.users.findOne({
+      where: { user_name: req.body.user_name }
+    });
+    if (registeredUser) {
+      throw new Error("That username has already been taken. Booooo!");
     }
-    res.redirect("/login?registeredSuccessfully=true")
+    let encrypted = bcrypt.hashSync(req.body.password, 10);
+    models.users.create({
+      user_name: req.body.user_name,
+      password: encrypted
+    });
+  } catch (e) {
+    res.send(e);
+  }
+  res.redirect("/login?registeredSuccessfully=true");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
-app.post("/loginUser", async (req, res)=>{
-    try{
-        let dbUser = await models.users.findOne({where: {user_name: req.body.user_name}});
-        if(!dbUser) throw new Error("Get outta herrreee!");
-        bcrypt.compare(req.body.password, dbUser.password, (err, same) =>{
-            if(err) throw err;
-            if(!same) throw new Error("Nope!");
-            req.session.user_id = dbUser.id;
-            res.redirect("/account/dashboard");
-            })
-    }catch(e){
-        res.send(e)
-    }
+app.post("/loginUser", async (req, res) => {
+  try {
+    let dbUser = await models.users.findOne({
+      where: { user_name: req.body.user_name }
+    });
+    if (!dbUser) throw new Error("Get outta herrreee!");
+    bcrypt.compare(req.body.password, dbUser.password, (err, same) => {
+      if (err) throw err;
+      if (!same) throw new Error("Nope!");
+      req.session.user_id = dbUser.id;
+      res.redirect("/account/dashboard");
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
-app.get("/about-us", function (req, res) {
-    res.render("about-us");
+app.get("/about-us", function(req, res) {
+  res.render("about-us");
 });
 
-app.get("/contact", function (req, res) {
-    res.render("contact");
+app.get("/contact", function(req, res) {
+  res.render("contact");
 });
 
 app.post("/account/login", (req, res) => {
-    res.redirect("/dashboard");
+  res.redirect("/dashboard");
 });
 
 app.listen(process.env.PORT || 8080);
